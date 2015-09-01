49cd434b2de3c392134866812cfa9a5c76b00bcf
joshuarule <joshua@joshuarule.com>
2015-08-31 20:23:52 -0400

    minor changes

--- core/quiz-controller.js
+++ core/quiz-controller.js
@@ -28,7 +28,7 @@ exports.QuizController = Montage.specialize(/** @lends QuizController# */ {
     constructor: {
         value: function(questionController, answerController) {
             this._questionController = questionController;
-            this._questions = questionController._questions;
+            // this._questions = questionController._questions;
             this._answerController = answerController;
             this._currentQuestionIndex = -1;
         }
@@ -49,6 +49,7 @@ exports.QuizController = Montage.specialize(/** @lends QuizController# */ {
     answer: {
         value: function(answer) {
             if (answer == this._currentQuestion.answer) {
+                console.log('true');
                 return true;
             } else {
                 return false;
