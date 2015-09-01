9be374dd364c6a428c31756829b5bcf8ad51f6d2
Pierre Chaussalet <pchaussalet@gmail.com>
2015-09-01 02:08:10 +0200

    Add answer-controller

---
+++ core/answer-controller.js
@@ -0,0 +1,22 @@
+/**
+ * @module answer-controller
+ */
+var Montage = require("montage/core/core").Montage,
+    Promise = require("montage/core/promise").Promise.Promise;
+/**
+ * @class AnswerController
+ * @extends Montage
+ */
+exports.AnswerController = Montage.specialize(/** @lends AnswerController# */ {
+    recordAnswer: {
+        value: function() {
+        }
+    }
+});
+
+
+
+
+
+
+
