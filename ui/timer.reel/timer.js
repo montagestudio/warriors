f86dc920502bf026c1302d0a6967dbd07dd03ec5
Pierre Chaussalet <pchaussalet@gmail.com>
2015-09-10 13:05:38 +0200

    Prefix timer value with 0 if < 10

--- ui/timer.reel/timer.js
+++ ui/timer.reel/timer.js
@@ -8,10 +8,17 @@ var Component = require("montage/ui/component").Component;
  * @extends Component
  */
 exports.Timer = Component.specialize(/** @lends Timer# */ {
-    constructor: {
-        value: function Timer() {
-            this.super();
+    _currentTime: {
+        value: null
+    },
+
+    currentTime: {
+        get: function() {
+            var prefix = this._currentTime < 10 ? '0' : '';
+            return prefix + this._currentTime;
+        },
+        set: function(time) {
+            this._currentTime = time;
         }
     }
-
 });
