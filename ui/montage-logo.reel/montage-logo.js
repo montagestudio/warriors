137ca35136530f6465517cf3e3ee90943c0ba939
joshuarule <joshua@joshuarule.com>
2015-09-23 01:23:53 -0400

    adding montage tag

---
+++ ui/montage-logo.reel/montage-logo.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/montage-logo.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class MontageLogo
+ * @extends Component
+ */
+exports.MontageLogo = Component.specialize(/** @lends MontageLogo# */ {
+    constructor: {
+        value: function MontageLogo() {
+            this.super();
+        }
+    }
+});
