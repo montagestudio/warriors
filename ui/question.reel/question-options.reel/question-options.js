34ac8db72344fa2d905a7aa37bf36922edd996af
joshuarule <joshua@joshuarule.com>
2015-10-12 10:41:11 -0400

    fixing rebase merge

--- ui/question.reel/question-options.reel/question-options.js
+++ ui/question.reel/question-options.reel/question-options.js
@@ -94,22 +94,69 @@ exports.QuestionOptions = Component.specialize(/** @lends QuestionOptions# */ {
                 i,
                 self = this;
 
-                halfPageWidth = 1300;
-                for (i = -1; i <= 9; i++) {
-                    angle = Math.PI - i * Math.PI / 8;
-                    point = this.scaleVector(this.pointInCircleAt(angle), halfPageWidth);
-                    tangent = this.scaleVector(this.tangentInCircleAt(angle), halfPageWidth * bezierHandlerLength);
-                    pagesKnots.push(
-                        {
-                            "knotPosition": [point[0], 0, point[1]],
-                            "previousHandlerPosition": [point[0] + tangent[0], 0, point[1] + tangent[1]],
-                            "nextHandlerPosition": [point[0] - tangent[0], 0, point[1] - tangent[1]],
-                            "previousDensity": 1.1,
-                            "nextDensity": 1.1,
-                            "rotateY": Math.PI/2 - angle
-                            // "opacity": 1 - Math.abs(i-4)*.2
-                        }
-                    );
+                if (window.innerWidth > 1000) {
+                    halfPageWidth = 1300;
+                    this.flowRibbon.cameraPosition = [0, 0, 2100];
+                    this.flowRibbon.cameraTargetPoint = [0, 0, 0];
+                    this.flowRibbon.cameraFov = 50;
+                    for (i = -1; i <= 9; i++) {
+                        angle = Math.PI - i * Math.PI / 8;
+                        point = this.scaleVector(this.pointInCircleAt(angle), halfPageWidth);
+                        tangent = this.scaleVector(this.tangentInCircleAt(angle), halfPageWidth * bezierHandlerLength);
+                        pagesKnots.push(
+                            {
+                                "knotPosition": [point[0], 0, point[1]],
+                                "previousHandlerPosition": [point[0] + tangent[0], 0, point[1] + tangent[1]],
+                                "nextHandlerPosition": [point[0] - tangent[0], 0, point[1] - tangent[1]],
+                                "previousDensity": 1.1,
+                                "nextDensity": 1.1,
+                                "rotateY": Math.PI/2 - angle
+                                // "opacity": 1 - Math.abs(i-4)*.2
+                            }
+                        );
+                    }
+                } else if (window.innerWidth > 600) {
+                    halfPageWidth = 1250;
+                    this.flowRibbon.cameraPosition = [0, 0, 2200];
+                    this.flowRibbon.cameraTargetPoint = [0, 0, 0];
+                    this.flowRibbon.cameraFov = 50;
+                    for (i = -1; i <= 9; i++) {
+                        angle = Math.PI - i * Math.PI / 8;
+                        point = this.scaleVector(this.pointInCircleAt(angle), halfPageWidth);
+                        tangent = this.scaleVector(this.tangentInCircleAt(angle), halfPageWidth * bezierHandlerLength);
+                        pagesKnots.push(
+                            {
+                                "knotPosition": [point[0], 0, point[1]],
+                                "previousHandlerPosition": [point[0] + tangent[0], 0, point[1] + tangent[1]],
+                                "nextHandlerPosition": [point[0] - tangent[0], 0, point[1] - tangent[1]],
+                                "previousDensity": 1.1,
+                                "nextDensity": 1.1,
+                                "rotateY": Math.PI/2 - angle
+                                // "opacity": 1 - Math.abs(i-4)*.2
+                            }
+                        );
+                    }
+                } else {
+                    halfPageWidth = 1200;
+                    this.flowRibbon.cameraPosition = [0, 0, 2350];
+                    this.flowRibbon.cameraTargetPoint = [0, 0, 0];
+                    this.flowRibbon.cameraFov = 50;
+                    for (i = -1; i <= 9; i++) {
+                        angle = Math.PI - i * Math.PI / 8;
+                        point = this.scaleVector(this.pointInCircleAt(angle), halfPageWidth);
+                        tangent = this.scaleVector(this.tangentInCircleAt(angle), halfPageWidth * bezierHandlerLength);
+                        pagesKnots.push(
+                            {
+                                "knotPosition": [point[0], 0, point[1]],
+                                "previousHandlerPosition": [point[0] + tangent[0], 0, point[1] + tangent[1]],
+                                "nextHandlerPosition": [point[0] - tangent[0], 0, point[1] - tangent[1]],
+                                "previousDensity": 1.1,
+                                "nextDensity": 1.1,
+                                "rotateY": Math.PI/2 - angle
+                                // "opacity": 1 - Math.abs(i-4)*.2
+                            }
+                        );
+                    }
                 }
 
                 this.flowRibbon.paths = [
@@ -169,12 +216,14 @@ exports.QuestionOptions = Component.specialize(/** @lends QuestionOptions# */ {
 
     templateDidLoad: {
         value: function () {
-
+            var self = this;
             this.setPaths();
 
             // handle resize function this.setPaths
 
-            window.addEventListener("resize", this, false);
+            window.addEventListener("resize", function () {
+                self.setPaths();
+            }, false);
         }
     }
 });
