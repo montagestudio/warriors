dd295b139bdc8614bc4eaa34c8e6ae7d114d1c64
Pierre Chaussalet <pchaussalet@gmail.com>
2015-10-16 04:26:23 +0200

    [local] Fix application dynamic configuration on mopped code

--- configure_warriors.sh
+++ configure_warriors.sh
@@ -2,10 +2,4 @@
 
 sleep 5
 QUIZ_ID=$(curl --data-binary @/srv/assets/quiz.json -H 'content-type: application/json' 127.0.0.1:3000/quiz)
-cat > /srv/server/static/core/configuration.js << EOF
-exports.configuration = {
-    backendUrl: 'http://localhost:3000',
-    quizId: '$QUIZ_ID',
-    quizTime: '60'
-};
-EOF
+sed -i "s/configuration={backendUrl:\"https:\/\/warriors.mybluemix.net\",quizId:\"1267b4bf-2669-4b48-a1a3-8838d2be50b0\",quizTime:\"60\"}/configuration={backendUrl:\"\",quizId:\"$QUIZ_ID\",quizTime:\"60\"}/" /srv/server/static/index.html.bundle-1-0.js
