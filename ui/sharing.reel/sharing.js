2ddc52d0c30e2059c4efaf62524383e93c7d1f3f
Benoit Marchant <marchant@mac.com>
2015-10-16 00:23:19 -0700

    Adds support for twitter sharing from montage-social and custom FB sharing, plus dependency on montage-social

--- ui/sharing.reel/sharing.js
+++ ui/sharing.reel/sharing.js
@@ -8,9 +8,17 @@ var Component = require("montage/ui/component").Component;
  * @extends Component
  */
 exports.Sharing = Component.specialize(/** @lends Sharing# */ {
-    constructor: {
-        value: function Sharing() {
-            this.super();
+    prepareForActivationEvents: {
+        value: function () {
+            this.facebookShareElement.addEventListener("click", this, false);
         }
-    }
+    },
+    handleClick: {
+        value: function (event) {
+            var dataObj = {'articleTitle':"How well do you know the 2015 NBA Champions?","image":window.location.href+"assets/image/CoverImage.png"};
+            var shareEvent = new CustomEvent('shareOnFB',{ 'detail': dataObj });
+            dispatchEvent(shareEvent);
+        }
+    },
+
 });
