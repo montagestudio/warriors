e60ccc5697e79558b91785e78e1f4ab9f2c7dd93
romancortes <javier.roman.c@gmail.com>
2015-10-23 19:41:42 +0200

    Finish Montage tag

--- ui/montage-tag.reel/montage-tag.js
+++ ui/montage-tag.reel/montage-tag.js
@@ -15,10 +15,24 @@ var MontageTag = exports.MontageTag = Button.specialize(/** @lends MontageTag# *
         value: true
     },
 
+    _collapseTimeout: {
+        value: null
+    },
+
     handleAction: {
         value: function () {
-            console.log('action');
+            var linkOpacity = getComputedStyle(this.linkElement).getPropertyValue("opacity") * 1,
+                self = this;
+
             this.classList.add("expanded");
+            clearTimeout(this._collapseTimeout);
+            this._collapseTimeout = setTimeout(function () {
+                self.classList.remove("expanded");
+            }, 5000);
+            if (linkOpacity > 0) {
+                window.open("http://www.montagestudio.com", '_blank');
+            }
         }
     }
+
 });
